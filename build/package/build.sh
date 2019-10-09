2d8a2c28ace44a9637731fb73fd25419fabf2a85
gbarbieru <gbarbieru@bitdefender.com>
2019-10-09 14:58:58 +0300

    fixed build script

--- build/package/build.sh
+++ build/package/build.sh
@@ -13,6 +13,7 @@ TARGET_DST="${BASE_DST}/opt/vagrant-private-cloud"
 #
 # create target directory
 #
+echo "Creating target directory"
 mkdir -p ${TARGET_DST}
 mkdir -p ${TARGET_DST}/src
 mkdir -p ${TARGET_DST}/config
@@ -20,6 +21,7 @@ mkdir -p ${TARGET_DST}/config
 #
 # copy application files
 #
+echo "Copying application files"
 cp -R ${REPO_SRC}/index.js      ${TARGET_DST}/index.js
 cp -R ${REPO_SRC}/package.json  ${TARGET_DST}/package.json
 cp -R ${REPO_SRC}/readme.md     ${TARGET_DST}/readme.md
@@ -30,23 +32,26 @@ cp -R ${REPO_SRC}/config/default.json ${TARGET_DST}/config/default.json
 #
 # copy systemd services
 #
+echo "Copying service file"
 mkdir -p "${BASE_DST}/lib/systemd/system/"
 cp -R "${REPO_SRC}/build/services/vagrant-private-cloud.service" "${BASE_DST}/lib/systemd/system/vagrant-private-cloud.service"
 
 #
 # copy dpkg scripts
 #
-mkdir -p ${BASE_DST}/opt/bitdefender/var/lib/dpkg/${PACKAGE_NAME}
+echo "Copying DPKG scripts"
+mkdir -p ${BASE_DST}/DEBIAN
 cp -R ${REPO_SRC}/build/package/deb/preinst  ${BASE_DST}/DEBIAN/preinst
 cp -R ${REPO_SRC}/build/package/deb/postinst ${BASE_DST}/DEBIAN/postinst
 cp -R ${REPO_SRC}/build/package/deb/prerm    ${BASE_DST}/DEBIAN/prerm
 cp -R ${REPO_SRC}/build/package/deb/postrm   ${BASE_DST}/DEBIAN/postrm
 cp -R ${REPO_SRC}/build/package/deb/control  ${BASE_DST}/DEBIAN/control
-chmod +x ${BASE_DST}/opt/bitdefender/var/lib/dpkg/${PACKAGE_NAME}/*
+chmod +x ${BASE_DST}/DEBIAN/*
 
 #
 # make sure all the interesting files are in unix format
 #
+echo "Ensuring line endings"
 find "$TARGET_DST" -name "*.sh" -print0 | xargs -0 dos2unix
 find "$TARGET_DST" -name "*.js" -print0 | xargs -0 dos2unix
 
@@ -60,5 +65,6 @@ PACKAGE_SIZE=$(du -s -k --exclude ${TARGET_DST}/DEBIAN* ${TARGET_DST} | cut -f1)
 #
 # build .deb
 #
-sed -i "5i Installed-Size: $PACKAGE_SIZE" ${TARGET_DST}/DEBIAN/control
-dpkg-deb --build ${TARGET_DST} $PACKAGE_NAME
+echo "Building .deb file"
+sed -i "5i Installed-Size: $PACKAGE_SIZE" ${BASE_DST}/DEBIAN/control
+dpkg-deb --build ${BASE_DST} $PACKAGE_FILENAME
